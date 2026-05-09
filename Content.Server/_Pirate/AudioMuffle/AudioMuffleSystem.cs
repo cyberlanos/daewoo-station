e371cdf7b2ce4067af3651813bd30b65f1bbe758
v0id <>
2026-05-09 16:30:00 +0300

    Revert "Merge pull request #507 from CyberLanos/feature/audio-muffle"
    This reverts commit cc0683cd606583fef68bdd4f32cf3a04d7cc769a, reversing
    changes made to c05ff6d06e1e4b8ff8c979cddabed4f46601eb66.

--- Content.Server/_Pirate/AudioMuffle/AudioMuffleSystem.cs
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
-using Content.Shared._Pirate.AudioMuffle;
-
-namespace Content.Server._Pirate.AudioMuffle;
-
-public sealed class AudioMuffleSystem : SharedAudioMuffleSystem;
