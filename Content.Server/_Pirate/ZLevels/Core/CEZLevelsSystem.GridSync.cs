32d4cc6b2178421134e6746af3c1d31a3a8e820d
CyberLanos <cyber.lanos00@gmail.com>
2026-05-25 15:48:06 +0200

    chore: revert broken fix

--- Content.Server/_Pirate/ZLevels/Core/CEZLevelsSystem.GridSync.cs
+++ Content.Server/_Pirate/ZLevels/Core/CEZLevelsSystem.GridSync.cs
@@ -110,31 +110,6 @@ public sealed partial class CEZLevelsSystem
     /// </summary>
     private void ApplyLinkage(Entity<CEZLevelsNetworkComponent> network, Dictionary<int, EntityUid> gridsByDepth)
     {
-        // Evict any grid that used to belong to THIS network but is no longer present in gridsByDepth,
-        // including the case where the network has shrunk to zero or one grid (which would otherwise
-        // early-out below and leave stale CEZLinkedGridComponent state behind).
-        var keepSet = new HashSet<EntityUid>(gridsByDepth.Values);
-        var orphanQuery = AllEntityQuery<CEZLinkedGridComponent>();
-        var orphans = new List<(EntityUid Uid, CEZLinkedGridComponent Comp)>();
-        while (orphanQuery.MoveNext(out var orphanUid, out var orphanComp))
-        {
-            if (orphanComp.ZNetwork != network.Owner)
-                continue;
-            if (keepSet.Contains(orphanUid))
-                continue;
-            orphans.Add((orphanUid, orphanComp));
-        }
-
-        foreach (var (orphanUid, orphanComp) in orphans)
-        {
-            UnlinkFromPriorNetwork(orphanComp);
-
-            if (orphanComp.Depth == 0)
-                _lastSyncedVelocity.Remove(orphanComp.ZNetwork);
-
-            RemComp<CEZLinkedGridComponent>(orphanUid);
-        }
-
         if (gridsByDepth.Count < 2)
             return;
 
